83cd7b6ca62d49780437f60e29183d1698b1865e
Nestiei <diogofrancisco1809@gmail.com>
2025-12-04 23:55:21 +0000

    feat: Complete part 2 of challenge 2.

--- 2025/02/main.go
+++ 2025/02/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func SplitLines(s string) []string {
-	return strings.Split(strings.TrimSpace(s), "\n")
+func SplitRanges(s string) []string {
+	return strings.Split(strings.TrimSpace(s), ",")
 }
 
 func ReadFileLines(path string) ([]string, error) {
@@ -18,14 +19,43 @@ func ReadFileLines(path string) ([]string, error) {
 	}
 	fileString := string(fileBytes)
 
-	return SplitLines(fileString), error
+	return SplitRanges(fileString), error
 }
 
 func main() {
-	// Read file lines.
-	lines, err := ReadFileLines("input.txt")
+	var total int = 0
+
+	// Read input.
+	ranges, err := ReadFileLines("input.txt")
 	if err != nil {
 		fmt.Println("Failed to read lines from file.")
 		return
 	}
+
+	// Process each range.
+	for _, idRange := range ranges {
+		fmt.Println(idRange)
+
+		rangeLimits := strings.Split(idRange, "-")
+		rangeMin, _ := strconv.Atoi(rangeLimits[0])
+		rangeMax, _ := strconv.Atoi(rangeLimits[1])
+
+		for number := rangeMin; number <= rangeMax; number++ {
+			numberString := fmt.Sprintf("%d", number)
+			numberStringLength := len(numberString)
+			if numberStringLength%2 != 0 {
+				continue
+			}
+
+			numberFirstHalf, _ := strconv.Atoi(numberString[:numberStringLength/2])
+			numberSecondHalf, _ := strconv.Atoi(numberString[numberStringLength/2:])
+
+			if numberFirstHalf == numberSecondHalf {
+				total += number
+				fmt.Println("Found invalid ID:", number)
+			}
+		}
+	}
+
+	fmt.Println("Total sum of invalid IDs:", total)
 }
