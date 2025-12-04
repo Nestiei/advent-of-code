60772ec5f81d682d5e5e70cbdf7a6bb612db4a3b
Nestiei <diogofrancisco1809@gmail.com>
2025-12-04 22:29:33 +0000

    feat: Complete part 1 of challenge 1.

---
+++ 2025/01/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func ReadFileLines(path string) ([]string, error) {
+	fileBytes, error := os.ReadFile(path)
+	if error != nil {
+		fmt.Println("Error reading file:", error)
+		return nil, error
+	}
+	fileString := string(fileBytes)
+
+	return SplitLines(fileString), error
+}
+
+func SplitLines(s string) []string {
+	return strings.Split(strings.TrimSpace(s), "\n")
+}
+
+func GetTurns(line string) (int, error) {
+	line = strings.TrimSpace(line)
+	number, err := strconv.Atoi(line[1:])
+	if err != nil {
+		return 0, err
+	}
+
+	switch {
+	case strings.HasPrefix(line, "L"):
+		return -1 * number, nil
+	case strings.HasPrefix(line, "R"):
+		return number, nil
+	}
+
+	return 0, fmt.Errorf("invalid line format")
+}
+
+func main() {
+	var dial int = 50
+	var password int = 0
+
+	// Read file lines.
+	lines, err := ReadFileLines("input.txt")
+	if err != nil {
+		fmt.Println("Failed to read lines from file.")
+		return
+	}
+
+	// Process each line.
+	for _, line := range lines {
+		turns, err := GetTurns(line)
+		if err != nil {
+			fmt.Println("Error parsing turns:", err)
+			return
+		}
+
+		dial = (dial + turns) % 100
+		fmt.Println("Dial rotated by", turns, "to position:", dial)
+		if dial == 0 {
+			password += 1
+			fmt.Println("Password incremented to:", password)
+		}
+	}
+
+	fmt.Println("Final Password:", password)
+}
